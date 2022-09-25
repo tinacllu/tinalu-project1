ed84d4ba0e2b72845f2309bf31589dc23ad3708d
tinacllu <tina.lu0107@gmail.com>
2022-09-24 21:55:44 -0400

    added right button functionality for img carousel

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// hamburger menu
 $(".hamburger").on("click", function () {
     if ($(".navLinks").css("display") == "none") {
         $(".navLinks").css({display: "block"});
@@ -6,7 +7,7 @@ $(".hamburger").on("click", function () {
     }
 });
 
-let mediaQuery = window.matchMedia('(max-width: 768px)');
+const mediaQuery = window.matchMedia('(max-width: 768px)');
 
 const displayNav = () => {
     if (mediaQuery.matches) {
@@ -17,3 +18,37 @@ const displayNav = () => {
 }
 
 window.addEventListener('resize', displayNav);
+
+// image carousel
+const nextArrow = document.querySelector(".next");
+const previousArrow = document.querySelector(".previous");
+const carousel = document.querySelector(".imgCarousel");
+const allImages = document.querySelectorAll(".image");
+let img1 = `<img src="./assets/gallery-image1.jpg" alt="house on the beach at sunset"></img>`
+let img2 = `<img src="./assets/gallery-image2.jpg" alt="walkway to a beach house">`
+let img3 = `<img src="./assets/gallery-image3.jpg" alt="house on the beach between palm trees"></img>`
+let counter = 0;
+const changeImage = () => {
+    console.log("test")
+}
+nextArrow.addEventListener("click", () => {
+    allImages[allImages.length - 1].innerHTML = allImages[0].innerHTML;
+
+    for (i = 0; i < allImages.length; i = i + 1) {
+        if (i < allImages.length - 1) {
+            allImages[i].innerHTML = allImages[i + 1].innerHTML;
+        } // else if (i === allImages.length - 1) {
+        //     console.log('else if statement');
+        // }
+    }
+  });
+
+previousArrow.addEventListener("click", () => {
+    console.log("left");
+    allImages[allImages.length-1].innerHTML = allImages[allImages.length - 2].innerHTML;
+    for (i = 0; i < allImages.length; i = i + 1) {
+        if (i < allImages.length - 1) {
+            allImages[i + 1].innerHTML = allImages[i].innerHTML;
+        }
+    }
+})
